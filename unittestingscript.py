569efd0b08f4d5f20982142bbd1f742a5dcd9838
ProfessorCancer <rizzleplaysgame@gmail.com>
2020-09-07 22:22:52 +0800

    My Python Libs

---
+++ unittestingscript.py
@@ -0,0 +1,12 @@
+import unittest
+import os
+
+class TestMyProgram(unittest.TestCase):
+    def test(self):
+        os.system("C:\\Users\\User\\PycharmProjects\\PY_Project\\PythonProjectTester.py")
+
+    if __name__ == '__main__':
+        unittest.main()
+
+process = unittest
+
